1099314b6b40e87959f746e43351c267cdace67d
Evgeniy Zhamkov <zhamkov92@inbox.ru>
2025-01-25 14:55:16 +0300

    first commit

--- src/main/java/ru/yandex/couriers/CourierActions.java
+++ src/main/java/ru/yandex/couriers/CourierActions.java
@@ -1,4 +1,43 @@
 package ru.yandex.couriers;
 
+import io.qameta.allure.Step;
+import io.restassured.http.ContentType;
+import io.restassured.response.ValidatableResponse;
+import io.restassured.specification.RequestSpecification;
+
+import static io.restassured.RestAssured.given;
+
 public class CourierActions {
+
+    public static RequestSpecification requestSpec() {
+        return given().log().all()
+                .contentType(ContentType.JSON)
+                .baseUri("https://qa-scooter.praktikum-services.ru/");
+    }
+
+    @Step("Регистрация нового курьера")
+    public ValidatableResponse createCourier(CreatingCourier creatingCourier) {
+        return requestSpec()
+                .body(creatingCourier)
+                .when()
+                .post("/api/v1/courier")
+                .then();
+    }
+
+    @Step("Авторизация курьера")
+    public ValidatableResponse loginCourier(CourierAuthorization courierАuthorization) {
+        return requestSpec()
+                .body(courierАuthorization)
+                .when()
+                .post("/api/v1/courier/login")
+                .then();
+    }
+
+    @Step("Удаление курьера")
+    public ValidatableResponse deleteCourier(int courierId) {
+        return requestSpec()
+                .when()
+                .delete("/api/v1/courier/:id" + courierId)
+                .then();
+    }
 }
--- src/main/java/ru/yandex/orders/CreateOrder.java
+++ src/main/java/ru/yandex/orders/CreateOrder.java
@@ -1,4 +1,43 @@
 package ru.yandex.orders;
 
+import io.qameta.allure.Step;
+import io.restassured.http.ContentType;
+import io.restassured.response.ValidatableResponse;
+import io.restassured.specification.RequestSpecification;
+
+import static io.restassured.RestAssured.given;
+
 public class CreateOrder {
+
+    public static RequestSpecification requestSpec() {
+        return given().log().all()
+                .contentType(ContentType.JSON)
+                .baseUri("https://qa-scooter.praktikum-services.ru/");
+    }
+
+    @Step("Создание заказа")
+    public ValidatableResponse createNewOrder(OrderDetails orderDetails) {
+        return requestSpec()
+                .body(orderDetails)
+                .when()
+                .post("/api/v1/orders")
+                .then();
+    }
+
+    @Step("Получение списка заказов")
+    public ValidatableResponse getOrderList() {
+        return requestSpec()
+                .when()
+                .get("/v1/orders")
+                .then();
+    }
+
+    @Step("Отмена заказа")
+    public ValidatableResponse cancelOrder(int track) {
+        return requestSpec()
+                .body(track)
+                .when()
+                .put("/api/v1/orders/cancel")
+                .then();
+    }
 }
